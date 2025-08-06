409ba6672e3b42ba6fdf1fbe8c259f32aa42245c
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-06 18:54:02 -0300

    busca dados do usuário

--- foodiary-api/src/controllers/MeController.ts
+++ foodiary-api/src/controllers/MeController.ts
@@ -1,10 +1,26 @@
+import { eq } from "drizzle-orm";
+import { db } from "../db";
 import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
 import { ok } from "../utils/http";
+import { usersTable } from "../db/schema";
 
 export class MeController {
     static async handle({ userId }: ProtectedHttpRequest): Promise<HttpResponse> {
+        const user = await db.query.usersTable.findFirst({
+            columns: {
+                id: true,
+                name: true,
+                email: true,
+                calories: true,
+                proteins: true,
+                carbohydrates: true,
+                fats: true
+            },
+            where: eq(usersTable.id, userId)
+        })
+
         return ok({
-            userId
+            user
         })
     }
 }
