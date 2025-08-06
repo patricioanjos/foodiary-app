b47008a9f46d2e98dd873a88e22316bde64c55d3
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-06 19:17:07 -0300

    cria tabela de refeições no banco

--- foodiary-api/src/db/schema.ts
+++ foodiary-api/src/db/schema.ts
@@ -1,12 +1,12 @@
-import { date, integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { date, integer, json, pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable('users', {
     id: uuid().primaryKey().defaultRandom(),
-    name: varchar({length: 255}).notNull(),
-    email: varchar({length: 255}).notNull().unique(),
-    password: varchar({length: 255}).notNull(),
-    goal: varchar({length:8}).notNull(),
-    gender: varchar({length: 6}).notNull(),
+    name: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({ length: 255 }).notNull(),
+    goal: varchar({ length: 8 }).notNull(),
+    gender: varchar({ length: 6 }).notNull(),
     birthDate: date('birth_date').notNull(),
     height: integer().notNull(),
     weight: integer().notNull(),
@@ -16,4 +16,21 @@ export const usersTable = pgTable('users', {
     proteins: integer().notNull(),
     fats: integer().notNull(),
     carbohydrates: integer().notNull(),
+})
+
+export const mealStatus = pgEnum('meal_status', ['uploading', 'success', 'failed', 'queued', 'processing'])
+export const mealInputType = pgEnum('meal_input_type', ['audio', 'picture'])
+
+export const mealsTable = pgTable('meals', {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid('user_id').notNull().references(
+        () => usersTable.id, { onDelete: 'cascade' }
+    ),
+    status: mealStatus().notNull(),
+    inputType: mealInputType('input_type').notNull(),
+    inputFileKey: varchar('input_file_key', { length: 255 }).notNull(),
+    name: varchar({ length: 255 }).notNull(),
+    icon: varchar({ length: 100 }).notNull(),
+    foods: json(),
+    createdAt: timestamp('created_at').notNull().defaultNow()
 })
