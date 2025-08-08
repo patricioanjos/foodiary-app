a21f4f48bdd86e12c6a294c0b3a182bf882f10bb
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-08 19:36:00 -0300

    implementa presigned PUT

--- foodiary-api/src/controllers/CreateMealController.ts
+++ foodiary-api/src/controllers/CreateMealController.ts
@@ -3,6 +3,10 @@ import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
 import { badRequest, created } from "../utils/http";
 import { db } from "../db";
 import { mealsTable } from "../db/schema";
+import { randomUUID } from "crypto";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { s3Client } from "../clients/s3Client";
 
 const schema = z.object({
     fileType: z.enum(['audio/m4a', 'image/jpg'])
@@ -16,9 +20,20 @@ export class CreateMealController {
             return badRequest({ errors: error.issues })
         }
 
+        const fileId = randomUUID()
+        const extension = data.fileType === 'audio/m4a' ? 'm4a' : 'jpg'
+        const fileKey = `${fileId}.${extension}`
+
+        const command = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: fileKey
+        })
+
+        const presignedURL = await getSignedUrl(s3Client, command, { expiresIn: 600 })
+
         const [meal] = await db.insert(mealsTable).values({
             userId,
-            inputFileKey: 'input_file_key',
+            inputFileKey: fileKey,
             inputType: data.fileType === 'audio/m4a' ? 'audio' : 'picture',
             status: 'uploading',
             name: '',
@@ -27,7 +42,7 @@ export class CreateMealController {
         }).returning({ id: mealsTable.id })
 
         return created({
-            mealId: meal.id
+            mealId: meal.id, uploadURL: presignedURL
         })
     }
 }
