bfd1e9cb1b8ada5ad0246978fe493f8b095bc703
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-09 10:22:29 -0300

    Cria o S3 trigger

---
+++ foodiary-api/src/functions/fileUploadedEvent.ts
@@ -0,0 +1,7 @@
+import { S3Event } from "aws-lambda";
+
+export async function handler(event: S3Event) {
+    await Promise.all(event.Records.map(record => {
+        record.s3.object.key
+    }))
+}
