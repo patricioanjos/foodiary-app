8ae1f0f1ff43badf938e60bc84d9a95079b0be06
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-09 11:33:24 -0300

    publica mensagens na fila

--- foodiary-api/src/functions/fileUploadedEvent.ts
+++ foodiary-api/src/functions/fileUploadedEvent.ts
@@ -1,7 +1,14 @@
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { S3Event } from "aws-lambda";
+import { sqsClient } from "../clients/sqsClient";
 
 export async function handler(event: S3Event) {
-    await Promise.all(event.Records.map(record => {
-        record.s3.object.key
+    await Promise.all(event.Records.map(async record => {
+        const command = new SendMessageCommand({
+            QueueUrl: process.env.MEALS_QUEUE_URL,
+            MessageBody: JSON.stringify({ fileKey: record.s3.object.key })
+        })
+
+        await sqsClient.send(command)
     }))
 }
