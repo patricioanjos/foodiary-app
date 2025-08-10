b9918b58399a9fe5cdd09312c9ae3ce3836ca3ea
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-10 16:56:36 -0300

    setup inicial

---
+++ foodiary/src/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import './styles/global.css';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
