f8b38bc18ff8d655386b8946cf0e2ca158afbb65
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-13 19:12:05 -0300

    ajusta posição dos botões da bottom bar

--- foodiary/src/components/MealBottomBar.tsx
+++ foodiary/src/components/MealBottomBar.tsx
@@ -7,18 +7,19 @@ export function MealBottomBar() {
     const { bottom } = useSafeAreaInsets()
     return (
         <View
-            className="sticky bg-white z-10 w-full h-20 bottom-0 border-t border-gray-400
-            flex-row justify-center pt-4 gap-4"
-            style={{marginBottom: bottom}}
+            className="sticky bg-white z-10 w-full h-20 bottom-0 border-t border-gray-400"
+            style={{ marginBottom: bottom }}
         >
 
-            <Button size="icon" color="gray">
-                <MicIcon />
-            </Button>
+            <View className="flex-row justify-center gap-4 mt-4">
+                <Button size="icon" color="gray">
+                    <MicIcon />
+                </Button>
 
-            <Button size="icon" color="gray">
-                <CameraIcon />
-            </Button>
+                <Button size="icon" color="gray">
+                    <CameraIcon />
+                </Button>
+            </View>
         </View>
     )
 }
