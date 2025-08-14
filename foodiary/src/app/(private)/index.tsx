35002453f78aa4a8b05dacacc83fd918920cfa48
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-14 19:48:32 -0300

    cria rotas públicas e privadas

---
+++ foodiary/src/app/(private)/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import HomeHeader from "../../components/HomeHeader";
+import { MealsList } from "../../components/MealsList";
+import { MealBottomBar } from "../../components/MealBottomBar";
+
+export default function Page() {
+  return (
+    <View className="flex-1 bg-white">
+      <HomeHeader />
+      <MealsList />
+
+      <MealBottomBar />
+    </View>
+  );
+}
