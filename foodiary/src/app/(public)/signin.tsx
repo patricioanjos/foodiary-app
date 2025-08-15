8c0924cbb3b6943f8f51fc593706a82c4821c766
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-15 11:02:08 -0300

    cria interface de login

--- foodiary/src/app/(public)/signin.tsx
+++ foodiary/src/app/(public)/signin.tsx
@@ -1,9 +1,41 @@
 import { Text, View } from "react-native";
+import { AuthLayout } from "../../components/AuthLayout";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { ArrowLeftIcon } from "lucide-react-native";
+import { router } from "expo-router";
 
 export default function SignIn() {
     return (
-        <View>
-            <Text>Entra...</Text>
-        </View>
+        <AuthLayout icon="👤" title="Entre em sua conta" subtitle="Acesse sua conta para continuar">
+            <View className="flex-1 justify-between">
+                <View className="gap-6">
+                    <Input
+                        label="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        autoComplete="email"
+                    />
+
+                    <Input
+                        label="Senha"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        autoComplete="password"
+                        secureTextEntry
+                    />
+                </View>
+
+                <View className="flex-row gap-6">
+                    <Button size="icon" color="gray" onPress={router.back}>
+                        <ArrowLeftIcon size={20} color={"#18181B"} />
+                    </Button>
+                    <Button className="flex-1">
+                        Entrar
+                    </Button>
+                </View>
+            </View>
+        </AuthLayout>
     )
 }
